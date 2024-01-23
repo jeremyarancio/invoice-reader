4727f0c438949fbf72bdf482a8addad1de65cbff
JeremyArancio <jeremyarancio@gmail.com>
2024-01-23 16:57:12 +0100

    Format validation string for comparison + map()

--- model/validation.py
+++ model/validation.py
@@ -1,7 +1,7 @@
 import logging
-from typing import Dict
+from typing import Dict, Mapping
 import json
-from tqdm import tqdm
+import re
 
 from datasets import load_dataset
 
@@ -22,6 +22,7 @@ class Validator:
             estimator: Estimator,
             streaming: bool = True
         ) -> Dict:
+        """"""
         validation = []
         test_dataset = load_dataset(
             path=self.validation_dataset,
@@ -29,20 +30,35 @@ class Validator:
             streaming=streaming
         )
         LOGGER.info(f"Dataset for validation: {test_dataset}")
-        for row in tqdm(test_dataset, ):
-            LOGGER.info(f"A row in the dataset: {row}")
-            ground_truth = json.loads(row["ground_truth"])
-            gt_total_gross_worth = ground_truth["gt_parse"]["summary"]["total_gross_worth"]
-            gt_total_gross_worth = self._process_ground_truth(gt_total_gross_worth)
-            predictions = estimator(row["image"])
-            for prediction in predictions:
-                total_gross_worth = prediction["answer"]
-                validation.append(total_gross_worth == gt_total_gross_worth)
+        updated_dataset = test_dataset.map(
+            self.map_function,
+            remove_columns="ground_truth"
+        )
+        for element in updated_dataset:
+            prediction = estimator(element["image"])[0]
+            LOGGER.info(f"Prediction during inference: {prediction}")
+            total_gross_worth = prediction["answer"]
+            total_gross_worth = self.process_amount(total_gross_worth)
+            LOGGER.debug(f"gt_total_gross_worth: {element['gt_total_gross_worth']}; predicted_total_gross_worth: {total_gross_worth}")
+            validation.append(total_gross_worth == element['gt_total_gross_worth'])
         return validation
     
+    def map_function(self, element: Mapping) -> Mapping:
+        """"""
+        ground_truth = json.loads(element["ground_truth"])
+        gt_total_gross_worth = ground_truth["gt_parse"]["summary"]["total_gross_worth"]
+        gt_total_gross_worth = self.process_amount(gt_total_gross_worth)
+        return {"gt_total_gross_worth": gt_total_gross_worth}
+
     @staticmethod
-    def _process_ground_truth(text: str) -> str:
-        text = text.replace("$ ", "")
+    def process_amount(text: str) -> str:
+        """Transform amounts (such as gross amount, net amout, ...) into an uniform for comparison.
+        
+        Example: '$45,54' or '$ 45. 54' -> 45.54
+        """
+        pattern = re.compile("(\d+)[.,](\d+)")
+        matches = pattern.search(text).groups()
+        text = matches[0] + "." + matches[1]
         return text
 
 
@@ -50,4 +66,4 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.WARNING)
     validator = Validator(validation_dataset="katanaml-org/invoices-donut-data-v1")
     estimator = Estimator()
-    validator(estimator=estimator)
+    validation = validator(estimator=estimator)
