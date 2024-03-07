4f9b0e9aa39c44796b5b2bac1a8746c9cec02951
JeremyArancio <jeremyarancio@gmail.com>
2024-03-07 13:01:31 +0100

    Deployment (WIP)

--- machine_learning/scripts/test_inference.py
+++ machine_learning/scripts/test_inference.py
@@ -1,35 +1,20 @@
-import base64
-from io import BytesIO
-from PIL import Image
+import logging
 
-from sagemaker.huggingface import HuggingFacePredictor
+from sagemaker.predictor import Predictor
 from sagemaker import Session
+from sagemaker.serializers import JSONSerializer
 
+# Temporary URLS
+PRESIGNED_URLS = [
+    "https://invoice-reader-project.s3.eu-central-1.amazonaws.com/data/training/documents/invoice_ext_25.png?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEMH%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaDGV1LWNlbnRyYWwtMSJGMEQCIAQFcQ0HwxPa9PfJUm64lRI6ID0I8ZD3YK2uJeMnV85dAiBk5YGJmwktRiu281LBFuXAMwxiqGlGyTo3y0KH%2B74kcyrtAgi6%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAEaDDI2NTg5MDc2MTc3NyIMTvltw0wEEIRGE131KsECo1oJ5eF%2FO5Od9avst5potWIVM7i%2FFSfLnsWzOEErhHybd%2B0YEFCp5o%2Fiy%2BwwWWAvp7%2Fv9Agy1gcviJeFLsDE1Mva7F10UglDon7EICXpygzSW92wH4hmy%2BQRODzLcQ2GkL9Lye78GIRA4W8roF8uKIL5OCS3MwvZkKxLKr6MS%2FjuEnRdOUB5YwIHHexoNvAAYMWwsRchIAvlOqESlKdfhdteJIucR1wEyE%2F0iUsf7c6BsmLVG8avG0OTNIUbMHHCiukPoQnmTnbo9fxyhd9XerWW2OfQ5Y1HSXunOci0xO%2FhKBaQWs6%2B0tAsOiBgo991A3dUsVpWFcS0b0wLkI8q1FZrvomp6dcULBXoF6g0NbEdRSKEBrrSXPWIuJwqx4eJi9ijOMr4B0HrYkRAw8E67JRnXa%2FxEK11wK4Zeaija3HIMLX%2Bpa8GOrQCQUuDOy3vdMUKKDUK595HgQ2pqzTlHTJ5LfoKan57B6F72hJyBEfWRB1oPSvklR9Q%2FcHeIC2i0cB2TwikJFfH6Kfd0PJiMojFNbpwRZQnZ2UrOnZ2KlSsQGGX8zWomDDX1HsE9NGyZDnt3gkdngd9GS2lbvmPpdGYZgmsmeGBrZevzdDEBo%2Bji7tqNU2r%2FiUHaEHrvKpvNcTroqN5HzcUTlp130v2Xc1iLYHfho%2BVD8oStAM8c9mcVv%2BUGwkV2k6%2BCVKojk6wXEXKUwHL56QYZRhA5mcjxWOC7UZFZeau7x7xexNciqJdK4HYywteoHR%2BZO%2FN7G0qRYO4gnB4ip0%2BQQ2dDX%2BYfyPsLRAhIe8rokcS0tPT3D9LnruBEme793tA6ynm9mn0V%2FYR4rexUYeS6NqpktY%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240307T113131Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=ASIAT32CTBQY3QRTAEAF%2F20240307%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Signature=330e487ad9697920a58014ab9f0304168e4f4738ad1d93ffb35211ea2a7747ed"
+]
 
-def convert_to_bytes(image: Image) -> str:
-    image = Image.open(".private/invoice.png")
-    buffered = BytesIO()
-    image.save(buffered, format="PNG")
-    img_str = base64.b64encode(buffered.getvalue()).decode("utf-8")
-    return img_str
+payload = {"urls": PRESIGNED_URLS}
 
-
-# create a serializer for the data
-# image_serializer = IdentitySerializer() # using x-image to support multiple image formats
-
-predictor = HuggingFacePredictor(
-    endpoint_name="huggingface-pytorch-inference-2024-01-31-10-38-00-160",
+predictor = Predictor(
+    endpoint_name="huggingface-pytorch-inference-2024-03-07-11-22-31-351",
     sagemaker_session=Session(),
-    # serializer=IdentitySerializer()
+    serializer=JSONSerializer()
 )
 
-image = Image.open(".private/invoice.png")
-prediction = predictor.predict(
-    {
-        "image": "https://www.invoicesimple.com/wp-content/uploads/2018/06/Sample-Invoice-printable.png",
-        "question": "What is the total gross amount?",
-    }
-)
-print(prediction)
-
-# ERROR: "message": "If you provide an image without word_boxes, then the pipeline will run OCR using Tesseract, but pytesseract is not available"
+print(predictor.predict(data=payload))
